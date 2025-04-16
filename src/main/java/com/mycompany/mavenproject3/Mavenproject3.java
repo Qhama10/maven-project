5cb82f2b7bf803fecb5e089772466299820784b0
Qhama10 <qhobongwana@gmail.com>
2025-04-16 14:58:32 +0200

    Update Mavenproject3.java

--- src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
+++ src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -12,18 +12,18 @@ public class Mavenproject3 {
 
     public static void main(String[] args) {
       
-//        String str = "Four score and seven years ago";
-//        int first, last;
-//        first = str.indexOf('r');
-//        last = str.lastIndexOf('r');
-//        System.out.println("The letter r first appears at" + "position" + first);
-//        System.out.println("The letter r last appears at " + "position " + last);
-//        
-//        String fullName = "Qhama Zintle Hobongwana";
-//        String lastName = fullName.substring(13);
-//        System.out.println("The full name is " + fullName);
-//        System.out.println("The last name is " + lastName);
-         System.out.println("Hello World!");
+        String str = "Four score and seven years ago";
+        int first, last;
+       first = str.indexOf('r');
+       last = str.lastIndexOf('r');
+       System.out.println("The letter r first appears at" + "position" + first);
+       System.out.println("The letter r last appears at " + "position " + last);
+        
+        String fullName = "Qhama Zintle Hobongwana";
+       String lastName = fullName.substring(13);
+        System.out.println("The full name is " + fullName);
+        System.out.println("The last name is " + lastName);
+         
     }
 }
         
